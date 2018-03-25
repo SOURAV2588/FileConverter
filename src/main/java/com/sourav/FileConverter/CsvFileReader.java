2718af4172746da2e9c3fa1d63affdbd1291626e
SOURAV2588 <souravghosh358@gmail.com>
2018-03-25 13:55:18 +0530

    Renamed the file.

--- src/main/java/com/sourav/FileConverter/CsvFileReader.java
+++ src/main/java/com/sourav/FileConverter/CsvFileReader.java
@@ -1,29 +1,55 @@
 package com.sourav.FileConverter;
 
+import com.sourav.FileConverter.model.Person;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.ResourceUtils;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
-public class SampleFileReader {
+public class CsvFileReader {
 
     @Test
     public void testRead(){
 
         try {
+            List<Person> personList = new ArrayList<>();
+            PersonToXml personToXml = new PersonToXml();
+
             //spring way(using resourceUtil) to read the file
             File file = ResourceUtils.getFile("classpath:MockData/Detailed person data with 8 fields.csv");
             BufferedReader bufferedReader  = null;
             String line = "";
             String csvSplitBy = ",";
             bufferedReader = new BufferedReader(new FileReader(file.toString()));
+            //This is to skip the first line of the csv file
+            //TODO: find an elegant solution
+            bufferedReader.readLine();
             while ((line = bufferedReader.readLine())!=null){
-                //line =bufferedReader.readLine();
                 String[] person = line.split(csvSplitBy);
-                //System.out.println("Person : Title: "+person[0]+" ,Firstname : "+person[1]+" Lastname :"+person[2]);
                 log.info("Person : Title: "+person[0]+" ,Firstname : "+person[1]+" Lastname :"+person[2]);
+
+                //setting all properties from the read file
+                Person readPerson = new Person();
+                readPerson.setTitle(person[0]);
+                readPerson.setFirstName(person[1]);
+                readPerson.setLastName(person[2]);
+                readPerson.setGender(person[3]);
+                readPerson.setSsn(person[4]);
+                readPerson.setPhone(Long.valueOf(person[5]));
+                readPerson.setStreet_address(person[6]);
+                readPerson.setEmail(person[7]);
+
+                //adding the person to the list
+                personList.add(readPerson);
             }
+
+            //converting to xml
+            personToXml.personToXmlConverter(personList);
+
             if (bufferedReader != null) {
                 try {
                     bufferedReader.close();
