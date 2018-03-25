91afdc32e44a5879c52b11844b5af4d5adf5f427
SOURAV2588 <souravghosh358@gmail.com>
2018-03-25 12:52:08 +0530

    Cleaned up application class

--- src/main/java/com/sourav/FileConverter/FileConverterApplication.java
+++ src/main/java/com/sourav/FileConverter/FileConverterApplication.java
@@ -3,11 +3,10 @@ package com.sourav.FileConverter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ResourceUtils;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
 
 @SpringBootApplication
 public class FileConverterApplication implements CommandLineRunner {
@@ -18,34 +17,5 @@ public class FileConverterApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings){
-		//give a path to a csv file
-        String csvFile = "F:/Test Mock Data/Detailed person data with 8 fields.csv";
-        BufferedReader bufferedReader  = null;
-        String line = "";
-        String csvSplitBy = ",";
-
-        try{
-            bufferedReader = new BufferedReader(new FileReader(csvFile));
-            while (bufferedReader.readLine()!=null){
-                line =bufferedReader.readLine();
-                String[] person = line.split(csvSplitBy);
-                System.out.println("Person : Title: "+person[0]+" ,Firstname : "+person[1]+" Lastname :"+person[2]);
-            }
-
-        }catch (FileNotFoundException e){
-            System.out.println("File not found");
-        }
-        catch (IOException e){
-            System.out.println("IOException");
-        }
-        finally {
-            if (bufferedReader != null) {
-                try {
-                    bufferedReader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
 	}
 }
